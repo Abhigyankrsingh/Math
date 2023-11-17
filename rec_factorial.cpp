d4033493047c124b5619072edac90ce4c7832a2f
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-17 15:03:55 +0530

    recursive Solution for factorial

---
+++ rec_factorial.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int rec(int n){
+
+    if(n <= 1){
+        return 1;
+    }
+
+    return n * rec(n-1);
+}
+
+
+
+int main(){
+
+int n =4;
+
+cout<<rec(n);
+
+}
